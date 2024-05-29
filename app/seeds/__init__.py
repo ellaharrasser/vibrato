00aaa2fbe4d5031fe6ab12516bf566e253d66203
Ethan Harrasser <ethan.harrasser@gmail.com>
2024-05-29 16:15:59 -0700

    Added product and product review seeders

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,8 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .shops import seed_shops, undo_shops
+from .products import seed_products, undo_products
+from .product_reviews import seed_product_reviews, undo_product_reviews
 
 from app.models.db import db, environment, SCHEMA
 
@@ -13,20 +15,22 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo
-        # command, which will  truncate all tables prefixed with
-        # the schema name (see comment in users.py undo_users function).
-        # Make sure to add all your other model's undo functions below
+        # Run seed undo and truncate tables before seeding in production
+        undo_product_reviews()
+        undo_products()
         undo_shops()
         undo_users()
+
     seed_users()
     seed_shops()
-    # Add other seed functions here
+    seed_products()
+    seed_product_reviews()
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_product_reviews()
+    undo_products()
     undo_shops()
     undo_users()
-    # Add other undo functions here
---
+++ app/seeds/product_reviews_seeds.py
@@ -0,0 +1,28 @@
+from ..models import ProductReview
+
+product_reviews = [
+    ProductReview(
+        user_id=1,
+        product_id=3,
+        review='Beautiful, beautiful guitar. Warm sound, super comfortable.',
+        rating=5,
+    ),
+    ProductReview(
+        user_id=2,
+        product_id=5,
+        review='Great MIDI keyboard for my software synths. A little flimsy.',
+        rating=4,
+    ),
+    ProductReview(
+        user_id=3,
+        product_id=8,
+        review='The harmonica is such a terrible instrument. Why did I buy this?',
+        rating=2,
+    ),
+    ProductReview(
+        user_id=3,
+        product_id=11,
+        review='Wonderful reverse feature, and the delay presets all sound very good!',
+        rating=5,
+    ),
+]
