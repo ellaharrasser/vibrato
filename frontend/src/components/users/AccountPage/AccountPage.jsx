c61eb11c180ad23022863c278dd34fb5cea15a3b
Ethan Harrasser <ethan.harrasser@gmail.com>
2024-06-07 10:08:35 -0700

    Added My Products section to AccountPage

--- frontend/src/components/users/AccountPage/AccountPage.jsx
+++ frontend/src/components/users/AccountPage/AccountPage.jsx
@@ -27,26 +27,29 @@ function AccountPage() {
     }, [user, dispatch, navigate]);
 
     const shops = useSelector(state => state.shops.shops);
-    // const products = useSelector(state => state.products.products);
+    const products = useSelector(state => state.products.products);
 
     return (
         <main>
-            <h1>My Account</h1>
-            <ul className='user-info'>
-                <li className='name'>{user.name}</li>
-                <li className='email'>{user.email}</li>
-                <li className='description'>{user.description}</li>
-                <li>
-                    <img
-                        src={user.profileImage}
-                        alt='User Image'
-                    />
-                </li>
-            </ul>
-            <h2>My Shops</h2>
-            {dataLoaded ? (
-                getKeys(shops).length ? (
-                    getValues(shops).map(shop => (
+            <div id='my-account-container'>
+                <h1>My Account</h1>
+                <ul className='user-info'>
+                    <li className='name'>{user.name}</li>
+                    <li className='email'>{user.email}</li>
+                    <li className='description'>{user.description}</li>
+                    <li>
+                        <img
+                            src={user.profileImage}
+                            alt='User Image'
+                        />
+                    </li>
+                </ul>
+            </div>
+            <div id='my-shops-container'>
+                <h2>My Shops</h2>
+                {dataLoaded ? (<>
+                    <NavLink to='/shops/new'>Create a Shop</NavLink>
+                    {getValues(shops).map(shop => (
                         // <MyShopCard shop={shop} />
                         <ul className='shop-card' key={shop.id}>
                             <li className='name'>{shop.name}</li>
@@ -55,12 +58,31 @@ function AccountPage() {
                                 <img
                                     src={shop.image}
                                     alt='Shop Image'
-                                />
+                                    />
                             </li>
                         </ul>
-                    ))
-                ) : <NavLink to='/shops/new'>Create your first Shop!</NavLink>
-            ) : <p className='loading'>Loading...</p>}
+                    ))}
+                </>) : <p className='loading'>Loading...</p>}
+            </div>
+            <div id='my-products-container'>
+                <h2>My Products</h2>
+                {dataLoaded ? (<>
+                    <NavLink to='/products/new'>Create a Product</NavLink>
+                    {getValues(products).map(product => (
+                        // <MyProductCard product={product} />
+                        <ul className='product-card' key={product.id}>
+                            <li className='name'>{product.name}</li>
+                            <li className='description'>{product.description}</li>
+                            <li>
+                                <img
+                                    src={product.images[0].image}
+                                    alt='Product Image'
+                                    />
+                            </li>
+                        </ul>
+                    ))}
+                </>) : <p className='loading'>Loading...</p>}
+            </div>
         </main>
     );
 }
