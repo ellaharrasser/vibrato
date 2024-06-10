2060a2aaa9d0ec3833c1b626a9c106ff8a239b2b
Ethan Harrasser <ethan.harrasser@gmail.com>
2024-06-09 23:58:03 -0700

    ProductDetailsPage done

--- frontend/src/components/products/ProductDetailsPage/ProductDetailsPage.jsx
+++ frontend/src/components/products/ProductDetailsPage/ProductDetailsPage.jsx
@@ -1,7 +1,8 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { NavLink, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { centsToUSD } from '../../../utils/misc';
 import { thunkLoadCurrentProduct } from '../../../redux/products';
 import OpenModalButton from '../../common/OpenModalButton';
 import EditProductModal from '../EditProductModal';
@@ -13,63 +14,79 @@ function ProductDetailsPage() {
     const dispatch = useDispatch();
     const { productId } = useParams();
 
-    const user = useSelector(state => state.session.user);
-    const product = useSelector(state => state.products.currentProduct);
+    const [dataLoaded, setDataLoaded] = useState(false);
 
     useEffect(() => {
         dispatch(thunkLoadCurrentProduct(productId));
+        setDataLoaded(true);
     }, [productId, dispatch]);
+
+    const user = useSelector(state => state.session.user);
+    const product = useSelector(state => state.products.currentProduct);
+
+    const productPriceText = centsToUSD(product.productPrice);
+    const shippingPriceText = (product.shippingPrice > 0)
+        ? `+ ${centsToUSD(product.shippingPrice)} Shipping`
+        : 'Free Shipping';
 
     return (
         <main id='product-details-page'>
             {product ? (
                 <>
-                    <div className='product-images-container'>
-                        <img
-                            className='product-image'
-                            src={product.images[0].image}
-                            alt='Product Image'
-                        />
-                    </div>
-                    <div className='product-info'>
-                        <h1>{product.brand} {product.name}</h1>
-                        <p className='category'>{product.category}</p>
-                        <p className='condition'>{product.condition}</p>
-                        <p className='price'>{product.productPrice}</p>
-                        <p className='shipping-price'>{product.shippingPrice}</p>
-                        <p className='description'>{product.description}</p>
-                        {user && product.user.id === user.id && (
-                            <div className='owner-actions'>
-                                <OpenModalButton
-                                    modalComponent={
-                                        <EditProductModal product={product}/>
-                                    }
-                                    buttonText={'Edit'}
-                                />
-                                <OpenModalButton
-                                    modalComponent={
-                                        <DeleteProductModal product={product}/>
-                                    }
-                                    buttonText={'Delete'}
-                                />
+                    <div className='product-wrapper'>
+                        <div className='product-images-container'>
+                            <img
+                                className='product-image'
+                                src={product.images[0].image}
+                                alt='Product Image'
+                            />
+                        </div>
+                        <div className='product-info'>
+                            <h1>{product.brand} {product.name}</h1>
+                            <p className='category'>{product.category}</p>
+                            <p className='condition'>{product.condition}</p>
+                            <div>
+                                <span className='price'>{productPriceText} </span>
+                                <span className='shipping-price'>{shippingPriceText}</span>
                             </div>
-                        )}
+                            <p className='description'>{product.description}</p>
+                            {user && product.user.id === user.id && (
+                                <div className='owner-actions'>
+                                    <OpenModalButton
+                                        modalComponent={
+                                            <EditProductModal product={product}/>
+                                        }
+                                        buttonText={'Edit'}
+                                    />
+                                    <OpenModalButton
+                                        modalComponent={
+                                            <DeleteProductModal product={product}/>
+                                        }
+                                        buttonText={'Delete'}
+                                    />
+                                </div>
+                            )}
+                        </div>
                     </div>
-                    <div className='shop-info'>
-                        <h2>About the Seller</h2>
-                        <img
-                            className='shop-image'
-                            src={product.shop.image}
-                            alt='Shop Image'
-                        />
-                        <p>{product.shop.name}</p>
-                        <NavLink to={`/shops/${product.shop.id}`}>
-                            View Shop
-                        </NavLink>
+                    <div className='shop-info-wrapper'>
+                        <h2>Sold By...</h2>
+                        <div className='shop-info'>
+                            <img
+                                className='shop-image'
+                                src={product.shop.image}
+                                alt='Shop Image'
+                            />
+                            <div className='shop-info-text'>
+                                <p className='shop-name'>{product.shop.name}</p>
+                                <NavLink to={`/shops/${product.shop.id}`}>
+                                    View Shop
+                                </NavLink>
+                            </div>
+                        </div>
                     </div>
                 </>
             ) : (
-                <p className='loading'>Loading...</p>
+                <h1 className='loading'>{dataLoaded ? 'No product found...' : 'Loading...'}</h1>
             )}
         </main>
     );
