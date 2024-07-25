7b3520d09259657effcee68e507b466fd6c70c92
Ethan Harrasser <ethan.harrasser@gmail.com>
2024-07-24 18:04:01 -0700

    category search bar converted

--- frontend/src/components/common/PageHeader/CategoryNavBar/CategoryNavBar.jsx
+++ frontend/src/components/common/PageHeader/CategoryNavBar/CategoryNavBar.jsx
@@ -1,7 +1,5 @@
 import { NavLink } from 'react-router-dom';
 
-// import './CategoryNavBar.css';
-
 
 function CategoryNavBar() {
     const comingSoonAlert = () => {
@@ -12,26 +10,31 @@ function CategoryNavBar() {
         <nav className='container'>
             <ul className='container flex flex-row flex-nowrap justify-center gap-4'>
                 <li
+                    className='text-base transition-colors hover:text-teal-400'
                     onClick={comingSoonAlert}
                 >
                     <NavLink to='/products?category=guitars'>Guitars</NavLink>
                 </li>
                 <li
+                    className='text-base transition-colors hover:text-teal-400'
                     onClick={comingSoonAlert}
                 >
                     <NavLink to='/products?category=keyboards_synths'>Keyboards and Synths</NavLink>
                 </li>
                 <li
+                    className='text-base transition-colors hover:text-teal-400'
                     onClick={comingSoonAlert}
                 >
                     <NavLink to='/products?category=effects_pedals'>Effects Pedals</NavLink>
                 </li>
                 <li
+                    className='text-base transition-colors hover:text-teal-400'
                     onClick={comingSoonAlert}
                 >
                     <NavLink to='/products?category=drums_percussion'>Drums and Percussion</NavLink>
                 </li>
                 <li
+                    className='text-base transition-colors hover:text-teal-400'
                     onClick={comingSoonAlert}
                 >
                     <NavLink to='/products?category=amplifiers'>Amplifiers</NavLink>
