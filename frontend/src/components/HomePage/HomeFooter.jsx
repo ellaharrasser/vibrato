e092dc378e2c9123d4727a1b46c5e757b34d327f
Ethan Harrasser <ethan.harrasser@gmail.com>
2024-09-18 17:12:41 -0700

    Changed GitHub and LinkedIn link urls

--- frontend/src/components/HomePage/HomeFooter.jsx
+++ frontend/src/components/HomePage/HomeFooter.jsx
@@ -2,13 +2,13 @@ function HomeFooter() {
   return (
     <footer className="w-[100%] h-8 fixed bottom-0 flex justify-center gap-8 text-center border-t border-stone-400 bg-[linear-gradient(10deg,_#99f6e4,_#34d399)]">
       <a
-        href="https://github.com/ethanharrasser"
+        href="https://github.com/ellaharrasser"
         className="my-auto text-lg font-semibold underline decoration-[rgba(0,_0,_0,_0)] decoration-2 underline-offset-2 transition-all hover:decoration-[rgba(0,_0,_0,_1)]"
       >
         GitHub
       </a>
       <a
-        href="https://www.linkedin.com/in/ethanharrasser"
+        href="https://www.linkedin.com/in/ellaharrasser"
         className="my-auto text-lg font-semibold underline decoration-[rgba(0,_0,_0,_0)] decoration-2 underline-offset-2 transition-all hover:decoration-[rgba(0,_0,_0,_1)]"
       >
         LinkedIn
